a8e16c0ad6989f21f852be0aee66d1df13fd08d6
Hannes Rantzsch <mail@hannesrantzsch.de>
2024-07-06 20:19:02 +0200

    refactoring for fun

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,27 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Main (main) where
 
 import Args
+import Control.Monad (when)
+import qualified Data.Bifunctor
 import qualified Data.List as L
-import qualified Data.MultiMap as M
-import Data.Traversable (forM)
+import qualified Data.Map.Lazy as M
+import qualified Data.MultiMap as MM
+import Data.Ord (comparing)
 import System.Directory
 import System.IO (hPutStrLn, stderr)
 import Text.Printf (printf)
-import Text.RE.TDFA.String (RE, matchedText, matches, re, (*=~), (?=~))
+import Text.RE.TDFA.String (matchedText, matches, re, (*=~), (?=~))
 
 type Tag = String
 
-type FileContent = String
+type Tags = MM.MultiMap Tag FilePath
 
-type Tags = M.MultiMap Tag FilePath
-
-fromLast :: Eq a => a -> [a] -> [a]
+fromLast :: (Eq a) => a -> [a] -> [a]
 fromLast x = reverse . takeWhile (/= x) . reverse
 
 extension :: FilePath -> String
@@ -38,40 +41,28 @@ getZettels f = map (joinPath f) . mdFiles <$> getDirectoryContents f
 
 getTags :: FilePath -> IO [Tag]
 getTags zettelPath = do
-  f <- readFile zettelPath
-  case extractTags f of
-    [] -> do
-      hPutStrLn stderr $ "No tags found in '" ++ zettelPath ++ "'"
-      return []
-    tags -> return tags
+  tags <- extractTags <$> readFile zettelPath
+  when (null tags) $ do
+    hPutStrLn stderr $ "No tags found in '" ++ zettelPath ++ "'"
+  return tags
   where
-    extractTags :: FileContent -> [Tag]
-    extractTags f = case matchedText $ f ?=~ matchTagsLine of
-      Just t -> matches $ t *=~ matchAllTags
-      Nothing -> []
-
-matchAllTags :: RE
-matchAllTags = [re|@[0-9a-zA-Z_-]+|]
-
-matchTagsLine :: RE
-matchTagsLine = [re|^tags: \[.*\]$|]
-
-asMap :: [(FilePath, [Tag])] -> Tags
-asMap tuples = M.fromList $ concatMap f tuples
-  where
-    f :: (FilePath, [Tag]) -> [(Tag, FilePath)]
-    f (_, []) = []
-    f (k, v : vs) = (v, k) : f (k, vs)
+    extractTags :: String -> [Tag]
+    extractTags = maybe [] matchTags . matchTagsLine
+    matchTags :: String -> [Tag]
+    matchTags s = matches $ s *=~ [re|@[0-9a-zA-Z_-]+|]
+    matchTagsLine :: String -> Maybe String
+    matchTagsLine s = matchedText $ s ?=~ [re|^tags: \[.*\]$|]
 
 allTags :: [FilePath] -> IO Tags
-allTags paths = do
-  tags <- forM paths $ \p -> do
-    ts <- getTags p
-    return (p, ts)
-  return $ asMap tags
+allTags ztFiles = asMap <$> mapM getTagsWithPath ztFiles
+  where
+    getTagsWithPath :: FilePath -> IO (FilePath, [Tag])
+    getTagsWithPath p = (p,) <$> getTags p
+    asMap :: [(FilePath, [Tag])] -> Tags
+    asMap tuples = MM.fromList [(tag, path) | (path, tags) <- tuples, tag <- tags]
 
-print_ :: Tags -> String
-print_ tagMap =
+printOverview :: Tags -> String
+printOverview tagMap =
   printf
     "Found %d Tags in %d Zettel.\n\
     \\n\
@@ -80,25 +71,23 @@ print_ tagMap =
     \Zettel by Tag:\n\
     \%s"
     (length tags)
-    (length . L.nub . concat . M.elems $ tagMap)
+    (length . L.nub . concat . MM.elems $ tagMap)
     (L.intercalate ", " tags) -- list of tags
     (L.intercalate "\n\n" (map showTag tags)) -- <tag>:\n<files>
   where
-    tags = M.keys tagMap
-    showTag t = "  " ++ t ++ ":\n    " ++ L.intercalate "\n    " (map basename $ M.lookup t tagMap)
+    tags = MM.keys tagMap
+    showTag t = "  " ++ t ++ ":\n    " ++ L.intercalate "\n    " (map basename $ MM.lookup t tagMap)
 
 printStats :: Tags -> String
-printStats tags = printf "%s" (L.intercalate "\n" $ map usage $ M.keys tags)
+printStats tags = L.intercalate "\n" $ map (uncurry (printf "%s: %d")) occurences
   where
-    usage :: Tag -> String
-    usage tag = printf "%s: %d" tag (length $ M.lookup tag tags)
+    occurences = L.sortBy (comparing snd) . map (Data.Bifunctor.second length) . M.toList $ MM.toMap tags
 
-run :: Zettel -> IO ()
-run (Zettel path' mode') = case mode' of
-  Overview -> putStrLn . print_ =<< allTags =<< getZettels path'
+run :: Zt -> IO ()
+run (Zt path' mode') = case mode' of
+  Overview -> putStrLn . printOverview =<< allTags =<< getZettels path'
   Stats -> putStrLn . printStats =<< allTags =<< getZettels path'
   Validate -> putStrLn "Not implemented yet"
 
 main :: IO ()
-main = do
-  parseArgs >>= run
+main = parseArgs >>= run
--- src/Args.hs
+++ src/Args.hs
@@ -1,30 +1,32 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
 {-# OPTIONS_GHC -fno-cse #-}
 
-module Args where
+module Args (Mode(..), Zt(..), parseArgs) where
 
-import System.Console.CmdArgs
+import System.Console.CmdArgs (Data, Typeable, cmdArgs, enum, explicit, help, helpArg, name, typDir, versionArg, (&=))
 
-data Mode'
+data Mode
   = Overview
   | Stats
   | Validate
   deriving (Show, Data)
 
-data Zettel = Zettel {path :: FilePath, mode :: Mode'}
+data Zt = Zt {path :: FilePath, mode :: Mode}
   deriving (Show, Data, Typeable)
 
-zettel =
-  Zettel
-    { path = "." &= typDir,
-      mode =
-        enum
-          [ Overview &= help "Print overview",
-            Stats &= help "Show stats",
-            Validate &= help "Check stuff"
-          ]
-    }
-    &= helpArg [explicit, name "h", name "help"]
-    &= versionArg [explicit, name "v", name "version"]
-
-parseArgs = cmdArgs zettel
+parseArgs :: IO Zt
+parseArgs =
+  cmdArgs
+    ( Zt
+        { path = "." &= typDir,
+          mode =
+            enum
+              [ Overview &= help "Print overview",
+                Stats &= help "Show stats",
+                Validate &= help "Check stuff"
+              ]
+        }
+        &= helpArg [explicit, name "h", name "help"]
+        &= versionArg [explicit, name "v", name "version"]
+    )
